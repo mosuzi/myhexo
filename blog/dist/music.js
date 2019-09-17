4bc3cd035aa23f48390a82bae06f131cdcbd7127
mosuzi <jingjie.song@definesys.com>
2019-09-17 22:24:26 +0800

    Site updated: 2019-09-17 22:24:25

--- blog/dist/music.js
+++ blog/dist/music.js
@@ -7,12 +7,12 @@ const ap = new APlayer({
       {
         name: "告白の夜",
         artist: 'Ayasa绚沙',
-        url: '/music/Ayasa.mp3'
+        url: '/blog/music/Ayasa.mp3'
       },
       {
         name: 'The dragonborn comes',
         artist: '括号君,迷惘（piano）',
-        url: '/music/The dragonborn comes.mp3',
+        url: '/blog/music/The dragonborn comes.mp3',
       }
     ]
 });
