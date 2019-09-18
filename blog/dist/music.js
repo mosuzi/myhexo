a838bc1f0d791408a2f712c8b2edf518067ec32f
mosuzi <jingjie.song@definesys.com>
2019-09-18 12:05:12 +0800

    Site updated: 2019-09-18 12:05:01

--- blog/dist/music.js
+++ blog/dist/music.js
@@ -1,7 +1,7 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: true,
+    autoplay: false,
     volume: 0.1,
     preload: 'metadata',
     audio: [
